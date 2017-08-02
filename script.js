da5986cd7fb7cfcdb403cbe99e1b4e1eaca1a130
Robin Zigmond <robinzig@hotmail.com>
2017-08-02 14:15:32 +0100

    added jquery rollover functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(".carousel").carousel({
     interval: 4000
 });
+
+$(window).on("load", function() {
+    $(".blog-image").hover(function() {
+        $(this).children(".fa").show();
+    }, function() {
+        $(this).children(".fa").hide();
+    });
+});
